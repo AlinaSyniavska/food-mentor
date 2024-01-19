bced16cee0b2ded52732c9db69d5f61201efcb99
AlinaSyniavska <alina22syniavska@gmail.com>
2024-01-19 10:58:36 +0200

    Corrected favicon src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+import Favicon from '/public/logo_icon.png';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -11,7 +13,7 @@ export const metadata: Metadata = {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/logo_icon.png',
+      url: Favicon.src,
     },
   ],
 };
